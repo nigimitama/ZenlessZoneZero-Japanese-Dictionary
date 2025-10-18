02d7ef3ba85f1d7f8bd8ee9f4e4c39006f91bd0b
mizma <omoikane@path-works.net>
2025-10-18 23:03:14 +0900

    出力ファイル名変更
    linux 用はmozc 向けのためmozc に名称変更

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from dict.words import RECORDS
 
 FILE_WIN = Path("./zzz-dict-win.txt")
-FILE_LIN = Path("./zzz-dict-linux.txt")
+FILE_LIN = Path("./zzz-dict-mozc.txt")
 FILE_MAC = Path("./zzz-dict-mac.txt")
 RELEASE_NOTE = Path("./GENERATED_RELEASE_NOTE.md")
 
