fcf7ca1390577275c72c65dc140af41ab4923e0b
mizma <omoikane@path-works.net>
2025-10-18 17:40:49 +0900

    2.3 主要用語追加
    - 2.3までのキャラクター名など追加
    - .gitignoreに生成物追加
    ｰ linux用にwinと同じ構造のUTF-8ファイル出力追加

--- dict/words.py
+++ dict/words.py
@@ -9,6 +9,7 @@ PARTIES = [
     Record(reading="たいほろうろっか", word="対ホロウ6課", category=Category.NOUN),
     Record(reading="かりゅどーんのこ", word="カリュドーンの子", category=Category.NOUN),
     Record(reading="おぼるすしょうたい", word="オボルス小隊", category=Category.NOUN),
+    Record(reading="かいたんや", word="怪啖屋", category=Category.NOUN),
 ]
 
 
@@ -23,6 +24,7 @@ ITEMS = [
     Record(reading="あられおつせいでん", word="あられ落つ星殿", category=Category.NOUN),
     Record(reading="せいでん", word="星殿", category=Category.NOUN),
     Record(reading="こくりゅう", word="刻流", category=Category.NOUN),
+    Record(reading="じょざい", word="助剤", category=Category.NOUN),
 ]
 
 
@@ -36,11 +38,19 @@ CHARACTERS = [
     Record(reading="しゅえん", word="朱鳶", category=Category.NAME),
     Record(reading="ちんいー", word="青衣", category=Category.NAME),
     Record(reading="そうかく", word="蒼角", category=Category.NAME),
+    Record(reading="ちーふーふー", word="橘福福", category=Category.NAME),
     Record(reading="ふーふー", word="福福", category=Category.NAME),
     Record(reading="ざお", word="照", category=Category.NAME),
     Record(reading="いーしぇん", word="儀玄", category=Category.NAME),
     Record(reading="ぱん", word="潘", category=Category.NAME),
     Record(reading="いんふー", word="引壺", category=Category.NAME),
+    Record(reading="うきなみゆずは", word="浮波柚葉", category=Category.NAME),
+    Record(reading="うきなみ", word="浮波", category=Category.NAME),
+    Record(reading="いぇーしゅんがん", word="葉瞬光", category=Category.NAME),
+    Record(reading="ようしゅんこう", word="葉瞬光", category=Category.NAME),
+    Record(reading="こまのまなと", word="狛野真斗", category=Category.NAME),
+    Record(reading="こまの", word="狛野", category=Category.NAME),
+    Record(reading="まなと", word="真斗", category=Category.NAME),
 ]
 
 
@@ -49,6 +59,7 @@ LOCATIONS = [
     Record(reading="ぜろごうほろう", word="零号ホロウ", category=Category.LOCATION),
     Record(reading="ろくぶんがい", word="六分街", category=Category.LOCATION),
     Record(reading="こくがんがい", word="黒雁街", category=Category.LOCATION),
+    Record(reading="えいひちく", word="衛非地区", category=Category.LOCATION),
 ]
 
 
@@ -60,6 +71,8 @@ OTHERS = [
     Record(reading="とうこう", word="刀耕", category=Category.NOUN),
     Record(reading="きゅうと", word="旧都", category=Category.NOUN),
     Record(reading="さんしょうかい", word="讃頌会", category=Category.NOUN),
+    Record(reading="いじょうしょうあく", word="異常掌握", category=Category.NOUN),
+    Record(reading="めいは", word="命破", category=Category.NOUN),
 ]
 
 
